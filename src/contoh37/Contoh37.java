7dbc35e7a88aa7490de45a9c4d31f9a4325b34cc
Asus <Asus@DESKTOP-4F3TTHE>
2019-02-13 10:23:30 +0800

    commit1

---
+++ src/contoh37/Contoh37.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contoh37;
+
+/**
+ *
+ * @author Asus
+ */
+public class Contoh37 {
+    public static void main(String[] args) {
+     int i;
+     int jumlah=0;
+     for(i=2;i<=100;i++){
+         jumlah = jumlah+i;
+     }
+     System.out.println("jumlah: "+jumlah);
+        // TODO code application logic here
+    }
+    
+}
